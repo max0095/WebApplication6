e05dfa32563ab211383787e5adc3b3c8da4d2c6c
Kitpipat Somprasong <maxnutnaro@gmail.com>
2017-11-20 15:39:24 +0700

    Add project files.

---
+++ WebApplication6/Migrations/201711200501283_gender.cs
@@ -0,0 +1,18 @@
+namespace WebApplication6.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class gender : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Gender", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Gender");
+        }
+    }
+}
